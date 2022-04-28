0b0974f3bd46d8057c9d2b7685cbe9735d7b2821
Sahachok.J <sahachok.j@kbtg.tech>
2022-04-28 15:09:56 +0700

    Working with props

--- src/workshop/Hello.jsx
+++ src/workshop/Hello.jsx
@@ -1,19 +1,20 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const Hello = () => {
-    const [name,setName] = useState("");
-    function handleName(event){
-        setName(event.target.value)
-    }
-    return (
-        <div>
-            <form>
-                Name : <input type="text" onChange={handleName} />
-            </form>
-            {/* Please enter your name */}
-            {name? <b>Hi, {name}</b>: "Please enter your name"}
-        </div>
-    )
-}
+const Hello = ({ initialName, onCall }) => {
+  const [name, setName] = useState("");
+  function handleName(event) {
+    onCall(event.target.value);
+    setName(event.target.value);
+  }
+  return (
+    <div>
+      <form>
+        Name : <input type="text" onChange={handleName} />
+      </form>
+      {/* Please enter your name */}
+      {name ? <b>Hi, {name}</b> : "Please enter your name"}
+    </div>
+  );
+};
 
-export default Hello
+export default Hello;
--- src/workshop/WorkShop.jsx
+++ src/workshop/WorkShop.jsx
@@ -1,12 +1,15 @@
-import Hello from "./Hello"
+import Hello from "./Hello";
 
 const Workshop = () => {
+  const callHandler = (message) => {
+    console.log("Call back with " + message);
+  };
   return (
     <div>
       <h2>Workshop component</h2>
-	  <Hello/>
+      <Hello initialName="Default name" onCall={callHandler} />
     </div>
-  )
-}
+  );
+};
 
-export default Workshop
+export default Workshop;
