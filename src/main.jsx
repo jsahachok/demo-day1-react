4ae6a893bae08c496bd6b25cca4e18c250aa4206
Sahachok.J <sahachok.j@kbtg.tech>
2022-04-28 14:53:25 +0700

    Workshop 01

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import AppClass from './AppClass'
+import WorkShop from './workshop/WorkShop'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <AppClass />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  //   <AppClass />
+  // </React.StrictMode>
   // normal mode
   //   <App />
   //   <AppClass />
+  <WorkShop/>
 )
---
+++ src/workshop/Hello.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const Hello = () => {
+    // useState("")
+    return (
+        <div>
+            <form>
+                Name : <input type="text" />
+            </form>
+            Please enter your name
+        </div>
+    )
+}
+
+export default Hello
