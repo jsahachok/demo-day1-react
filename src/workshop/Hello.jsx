e56d91a83e320f087fd7b841989c398b45f6a7e4
Sahachok.J <sahachok.j@kbtg.tech>
2022-04-28 15:02:49 +0700

    useState with name

--- src/workshop/Hello.jsx
+++ src/workshop/Hello.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react"
 
 const Hello = () => {
-    // useState("")
+    const [name,setName] = useState("");
+    function handleName(event){
+        setName(event.target.value)
+    }
     return (
         <div>
             <form>
-                Name : <input type="text" />
+                Name : <input type="text" onChange={handleName} />
             </form>
-            Please enter your name
+            {/* Please enter your name */}
+            {name? <b>Hi, {name}</b>: "Please enter your name"}
         </div>
     )
 }
