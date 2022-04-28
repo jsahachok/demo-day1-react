7fe542619b46cf91ef3a7c9a5afe2a522fbec9ee
Sahachok.J <sahachok.j@kbtg.tech>
2022-04-28 12:02:40 +0700

    Setup git + function component

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const message = "Hello Function Component!!"
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{{message}}</p>
+        <p>
+          <button type="button" 
+            onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
